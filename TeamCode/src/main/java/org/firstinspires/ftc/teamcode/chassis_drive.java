b99fd433438c82c6659bedc8104100550dd728c7
JacobGowan99bacon <45701922+JacobGowan99bacon@users.noreply.github.com>
2019-01-19 13:41:11 -0500

    i added a program where the robot moves forward and one where the linear slide moves up and down

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chassis_drive.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+public class chassis_drive extends LinearOpMode {
+
+
+
+        @Override
+        public void runOpMode() {
+            telemetry.addData("Status", "Initialized");
+            telemetry.update();
+            // Wait for the game to start (driver presses PLAY)
+            waitForStart();
+
+
+
+            // run until the end of the match (driver presses STOP)
+            while (opModeIsActive()) {
+                telemetry.addData("Status", "Running");
+                telemetry.update();
+
+            }
+
+        }
+
+    }
+
