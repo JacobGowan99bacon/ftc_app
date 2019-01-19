76a086d131b80a5a86e5a9675532f01013290059
JacobGowan99bacon <45701922+JacobGowan99bacon@users.noreply.github.com>
2019-01-19 15:02:05 -0500

    i did more stuff

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Linear.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Linear.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
+/*/* Copyright (c) 2017 FIRST. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted (subject to the limitations in the disclaimer below) provided that
@@ -112,3 +112,4 @@ public class BasicOpMode_Linear extends LinearOpMode {
         }
     }
 }
+
