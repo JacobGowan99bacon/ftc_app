5a46ed5aa6271c89d0c1ac8606fef18d4067e0da
JacobGowan99bacon <45701922+JacobGowan99bacon@users.noreply.github.com>
2019-01-22 17:13:25 -0500

    i added a file called autonomousrex

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomousrex.java
@@ -0,0 +1,107 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
+
+
+@Autonomous(name="Autonomousrex", group="Pushbot")
+//@Disabled
+public class Autonomousrex extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    HardwarePushbot robot = new HardwarePushbot();   // Use a Pushbot's hardware
+    private ElapsedTime runtime = new ElapsedTime();
+
+    static final double COUNTS_PER_MOTOR_REV = 1440;    // eg: TETRIX Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 2.0;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double DRIVE_SPEED = 0.5;
+    static final double TURN_SPEED = 0.5;
+    //private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftDrive = hardwareMap.get(DcMotor.class, "linearslideleft");
+        rightDrive = hardwareMap.get(DcMotor.class, "linearslideright");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+
+
+        // Step 1:  Drive forward for 3 seconds
+        while (opModeIsActive() && (runtime.seconds() < 4.0)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+            leftDrive.setPower(DRIVE_SPEED);
+            rightDrive.setPower(DRIVE_SPEED);
+        }
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(250);
+        // Setup a variable for each drive wheel to save power level for telemetry
+
+
+        // Step 1:  Drive forward for 3 seconds
+        while (opModeIsActive() && (runtime.seconds() < 0.6)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+            leftDrive.setPower(DRIVE_SPEED);
+            rightDrive.setPower(DRIVE_SPEED);
+        }
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(250);
+
+        // Step 1:  Drive forward for 3 seconds
+        while (opModeIsActive() && (runtime.seconds() < 0.6)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+            leftDrive.setPower(DRIVE_SPEED);
+            rightDrive.setPower(DRIVE_SPEED);
+        }
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(250);
+        // Setup a variable for each drive wheel to save power level for telemetry
+
+    }
+}
