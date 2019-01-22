49c16a924c4cd006f1448ebe165915e1c77b3076
JacobGowan99bacon <45701922+JacobGowan99bacon@users.noreply.github.com>
2019-01-22 13:23:46 -0500

    i made the robot drive forwards for half a second

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autotonomousrex.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autotonomousrex.java
@@ -17,35 +17,73 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 public class Autotonomousrex extends LinearOpMode {
 
     /* Declare OpMode members. */
-    HardwarePushbot         robot   = new HardwarePushbot();   // Use a Pushbot's hardware
-    private ElapsedTime     runtime = new ElapsedTime();
+    HardwarePushbot robot = new HardwarePushbot();   // Use a Pushbot's hardware
+    private ElapsedTime runtime = new ElapsedTime();
 
-    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+    static final double COUNTS_PER_MOTOR_REV = 1440;    // eg: TETRIX Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 2.0;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double     DRIVE_SPEED             = 0.5;
-    static final double     TURN_SPEED              = 0.5;
+    static final double DRIVE_SPEED = 0.5;
+    static final double TURN_SPEED = 0.5;
     //private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
 
     @Override
-        public void runOpMode() {
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftDrive = hardwareMap.get(DcMotor.class, "linearslideleft");
+        rightDrive = hardwareMap.get(DcMotor.class, "linearslideright");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+
+
+        // Step 1:  Drive forward for 3 seconds
+        while (opModeIsActive() && (runtime.seconds() < 5.0)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+            leftDrive.setPower(DRIVE_SPEED);
+            rightDrive.setPower(DRIVE_SPEED);
+        }
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(1000);
+        // Setup a variable for each drive wheel to save power level for telemetry
+
             telemetry.addData("Status", "Initialized");
             telemetry.update();
 
             // Initialize the hardware variables. Note that the strings used here as parameters
             // to 'get' must correspond to the names assigned during the robot configuration
             // step (using the FTC Robot Controller app on the phone).
-                  leftDrive  = hardwareMap.get(DcMotor.class, "linearslideleft");
-        rightDrive = hardwareMap.get(DcMotor.class, "linearslideright");
+            leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
+            rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
             // Most robots need the motor on one side to be reversed to drive forward
             // Reverse the motor that runs backwards when connected directly to the battery
             leftDrive.setDirection(DcMotor.Direction.FORWARD);
-            rightDrive.setDirection(DcMotor.Direction.REVERSE);
+            rightDrive.setDirection(DcMotor.Direction.FORWARD);
 
             // Wait for the game to start (driver presses PLAY)
             waitForStart();
@@ -54,26 +92,27 @@ public class Autotonomousrex extends LinearOpMode {
             // run until the end of the match (driver presses STOP)
 
 
-
-        // Step 1:  Drive forward for 3 seconds
-                while (opModeIsActive() && (runtime.seconds() < 5.0)) {
-                    telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
-                    telemetry.update();
-                    leftDrive.setPower(DRIVE_SPEED);
-                    rightDrive.setPower(DRIVE_SPEED);
-                }
-
-                leftDrive.setPower(0);
-                rightDrive.setPower(0);
-
-
-                telemetry.addData("Path", "Complete");
+            // Step 1:  Drive forward for 3 seconds
+            while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+                telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
                 telemetry.update();
-                sleep(1000);
-                // Setup a variable for each drive wheel to save power level for telemetry
-
+                leftDrive.setPower(DRIVE_SPEED);
+                rightDrive.setPower(DRIVE_SPEED);
             }
+
+            leftDrive.setPower(0);
+            rightDrive.setPower(0);
+
+
+            telemetry.addData("Path", "Complete");
+            telemetry.update();
+            sleep(1000);
+            // Setup a variable for each drive wheel to save power level for telemetry
+
         }
+    }
+
+
 
 
 
