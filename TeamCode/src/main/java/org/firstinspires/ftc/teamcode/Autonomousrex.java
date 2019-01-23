db47227856313838ba9603ff99f7cc5d21f8ac1e
JacobGowan99bacon <45701922+JacobGowan99bacon@users.noreply.github.com>
2019-01-22 20:06:14 -0500

    i removed a bunch of comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomousrex.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomousrex.java
@@ -15,7 +15,6 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 //@Disabled
 public class Autonomousrex extends LinearOpMode {
 
-    /* Declare OpMode members. */
     HardwarePushbot robot = new HardwarePushbot();   // Use a Pushbot's hardware
     private ElapsedTime runtime = new ElapsedTime();
 
@@ -26,7 +25,6 @@ public class Autonomousrex extends LinearOpMode {
             (WHEEL_DIAMETER_INCHES * 3.1415);
     static final double DRIVE_SPEED = 0.5;
     static final double TURN_SPEED = 0.5;
-    //private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
 
@@ -35,24 +33,17 @@ public class Autonomousrex extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
+
         leftDrive = hardwareMap.get(DcMotor.class, "linearslideleft");
         rightDrive = hardwareMap.get(DcMotor.class, "linearslideright");
 
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
+
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD);
-        // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
 
-        // run until the end of the match (driver presses STOP)
 
-
-        // Step 1:  Drive forward for 3 seconds
         while (opModeIsActive() && (runtime.seconds() < 4.0)) {
             telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
             telemetry.update();
@@ -67,10 +58,8 @@ public class Autonomousrex extends LinearOpMode {
         telemetry.addData("Path", "Complete");
         telemetry.update();
         sleep(250);
-        // Setup a variable for each drive wheel to save power level for telemetry
 
 
-        // Step 1:  Drive forward for 3 seconds
         while (opModeIsActive() && (runtime.seconds() < 0.6)) {
             telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
             telemetry.update();
@@ -86,7 +75,6 @@ public class Autonomousrex extends LinearOpMode {
         telemetry.update();
         sleep(250);
 
-        // Step 1:  Drive forward for 3 seconds
         while (opModeIsActive() && (runtime.seconds() < 0.6)) {
             telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
             telemetry.update();
@@ -101,7 +89,6 @@ public class Autonomousrex extends LinearOpMode {
         telemetry.addData("Path", "Complete");
         telemetry.update();
         sleep(250);
-        // Setup a variable for each drive wheel to save power level for telemetry
 
     }
 }
