b04c8113a7c7493b66310408d4d0f219da38f01e
JacobGowan99bacon <45701922+JacobGowan99bacon@users.noreply.github.com>
2019-01-22 20:07:26 -0500

    i removed some useless lines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomousrex.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomousrex.java
@@ -1,11 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 
