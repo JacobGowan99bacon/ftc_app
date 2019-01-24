36e3fbbf1c5aa9a9c6deb9de6ea894a256a06e3b
JacobGowan99bacon <45701922+JacobGowan99bacon@users.noreply.github.com>
2019-01-24 12:45:03 -0500

    i added a just in case teleop and a temporary autonomos that will be deleted

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/justincasecontrollerbreaks.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/justincasecontrollerbreaks.java
@@ -1,20 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
-        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-        import com.qualcomm.robotcore.hardware.DcMotor;
-        import com.qualcomm.robotcore.hardware.DcMotorSimple;
-        import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
-@TeleOp(name="Driver", group="Linear Opmode")
-public class Driver extends LinearOpMode {
+@TeleOp(name="justincasecontrollerbreaks", group="Linear Opmode")
+public class justincasecontrollerbreaks extends LinearOpMode {
 
     // public void Driver() {
     private DcMotor leftDrive;
     private DcMotor rightDrive;
     private DcMotor leftSlide;
     private DcMotor rightSlide;
-    // private Gamepad gamepad1 = new Gamepad;
 
 
     @Override
@@ -33,14 +32,12 @@ public class Driver extends LinearOpMode {
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
 
-        // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
-        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            double power = gamepad2.right_stick_y;
-            double rightmotorpower = gamepad1.right_stick_y;
-            double leftmotorpower = gamepad1.left_stick_y;
+            double power = gamepad1.right_stick_y;
+            double rightmotorpower = gamepad1.right_trigger;
+            double leftmotorpower = gamepad1.left_trigger;
             leftSlide.setPower(power);
             rightSlide.setPower(power);
 
