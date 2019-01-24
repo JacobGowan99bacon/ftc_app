6d10f49c58bd5590cb8f10b1963a0a62cd98c6b0
JacobGowan99bacon <45701922+JacobGowan99bacon@users.noreply.github.com>
2019-01-23 20:01:57 -0500

    i adjusted 2 more times to be less

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autolongway.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autolongway.java
@@ -69,7 +69,7 @@ public class Autolongway extends LinearOpMode {
         sleep(250);
 
         runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+        while (opModeIsActive() && (runtime.seconds() < 0.25)) {
             telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
             telemetry.update();
             leftDrive.setPower(LANDER_SPEED);
@@ -94,7 +94,7 @@ public class Autolongway extends LinearOpMode {
 
 
         runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 1.0)) {
+        while (opModeIsActive() && (runtime.seconds() < 0.7)) {
             telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
             telemetry.update();
             leftDrive.setPower(-LANDER_SPEED);
